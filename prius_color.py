5c85accfdd6181120cbf253c000b5bae24395e24
Christopher Highman <splices0@gmail.com>
2020-05-03 20:56:38 -0700

    Add files via upload

--- prius_color.py
+++ prius_color.py
@@ -86,7 +86,7 @@ def write_json(data, filename="color_counts.json"):
 
 
 def find_significant_contour(img):
-	image, contours, hierarchy = cv2.findContours(
+	contours, hierarchy = cv2.findContours(
 		img,
 		cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE
